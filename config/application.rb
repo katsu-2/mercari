e77981b1da0b6430d40f7c420e48dfd05c9cc691
大平勝也 <k8t2y9.cat@gmail.com>
2019-04-08 14:58:55 +0900

    feat: userファクトリーの追加

--- config/application.rb
+++ config/application.rb
@@ -12,7 +12,6 @@ module Mercari
     config.load_defaults 5.2
     config.generators do |g|
       g.test_framework :rspec,
-        fixtures: false,
         view_specs: false,
         helper_specs: false,
         routing_specs: false
