f359d743af0eee581f8d807ec0d7b6a70dc3567c
大平勝也 <k8t2y9.cat@gmail.com>
2019-04-08 16:13:21 +0900

    feat: deviseの日本語化

--- config/application.rb
+++ config/application.rb
@@ -9,6 +9,7 @@ Bundler.require(*Rails.groups)
 module Mercari
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
+    config.i18n.default_locale = :ja
     config.load_defaults 5.2
     config.generators do |g|
       g.test_framework :rspec,
