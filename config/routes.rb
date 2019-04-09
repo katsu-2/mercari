32961c2f68e19bf55ff8846db3c6421aaeb9f4c5
大平勝也 <k8t2y9.cat@gmail.com>
2019-04-09 17:24:16 +0900

    feat: controllers/users/registrations_controllerの作成

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,12 @@
 Rails.application.routes.draw do
   devise_for :users
+  devise_scope :user do
+    #サインアップ
+    get "signup" => "users/registrations#signup"
+  end
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root "items#index"
   resources :users
   resources :items, only:[:index]
+
 end
