b3192adfd83cdf6e26af9c8f308b62d56f835cd9
大平勝也 <k8t2y9.cat@gmail.com>
2019-04-10 12:21:35 +0900

    feat: ログインと新規登録のパス変更

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,11 @@
 Rails.application.routes.draw do
-  devise_for :users
+  devise_for :users, skip: :all
   devise_scope :user do
-
+    #ログイン
+    get "login" => "users/sessions#new", as: :new_user_session
     #サインアップ
-    get "signup" => "users/registrations#signup"
+    get  "signup" => "users/registrations#signup"
+    post "signup/registration" => "users/registrations#registration"
   end
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root "items#index"
