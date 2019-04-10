06b1db6e95726e958b04b3b2799807f0aa004a77
tunakan27 <takumi.naito27@gmail.com>
2019-04-10 11:10:47 +0900

    feat:  userのルーティングを設定する

--- config/routes.rb
+++ config/routes.rb
@@ -7,7 +7,7 @@ Rails.application.routes.draw do
   end
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root "items#index"
-  resources :users
+  resources :users, only:[:show]
   resources :items, only:[:index]
 
 end
