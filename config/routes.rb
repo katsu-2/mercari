f0ec76e6f1d5dea4a56b90410c0f67dd11155e49
orix1944 <bf109.sai@gmail.com>
2019-04-11 17:30:59 +0900

    create-logout-page

--- config/routes.rb
+++ config/routes.rb
@@ -9,10 +9,13 @@ Rails.application.routes.draw do
   end
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root "items#index"
-  resources :users, only:[:show] do
+  resources :users, only:[:show,] do
    resource :profiles, only:[:show]
    end
   resources :items, only:[:index]
+
+get "logout" => 'logouts#destory'
+
 
 
 end
