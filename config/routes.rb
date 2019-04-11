2ebe11489e5e35f5d61eeb4ecd4c20c21937153e
大平勝也 <k8t2y9.cat@gmail.com>
2019-04-11 14:36:15 +0900

    fix: ログイン、新規登録ページはdeviseにあるアクションを使用

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,10 @@
 Rails.application.routes.draw do
-  devise_for :users, skip: :all
+  devise_for :users
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root "items#index"
   resources :users
   resources :items, only:[:index]
-  resources :sessions, only: [:new, :create]
   resources :signups, only: [:new, :create]
   resources :registrations, only: [:new, :create]
   resources :cellphones, only: [:new, :create]
