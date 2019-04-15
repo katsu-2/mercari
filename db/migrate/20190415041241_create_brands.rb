07cbec9d086f2bcf9b81599cba09b7fb4bd469af
大平勝也 <k8t2y9.cat@gmail.com>
2019-04-15 14:10:59 +0900

    fix: インデント修正

--- db/migrate/20190415041241_create_brands.rb
+++ db/migrate/20190415041241_create_brands.rb
@@ -1,7 +1,7 @@
 class CreateBrands < ActiveRecord::Migration[5.2]
   def change
     create_table :brands do |t|
-      t.string :brand
+      t.string     :brand
       t.references :category, foreign_key: true
 
       t.timestamps
