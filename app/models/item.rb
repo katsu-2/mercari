89d2819cbf54eacade8ac80a9235067dfc68cbe7
大平勝也 <k8t2y9.cat@gmail.com>
2019-04-15 13:32:42 +0900

    feat: itemモデルからのアソシエーション追加

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,2 +1,10 @@
 class Item < ApplicationRecord
+  has_many   :comments
+  has_many   :brands
+  has_many   :images
+  has_many   :profits
+  has_many   :likes
+  belongs_to :user
+  belongs_to :category
+  belongs_to :cart
 end
