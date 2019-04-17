5b6e136b5c6e5720f419566a542f4644994b62fb
tunakan27 <takumi.naito27@gmail.com>
2019-04-17 11:46:11 +0900

    feat: active_storageを用いて:imageを追加

--- app/models/item.rb
+++ app/models/item.rb
@@ -7,6 +7,7 @@ class Item < ApplicationRecord
   # belongs_to :user
   # belongs_to :category
   # belongs_to :cart
+  has_one_attached :image
 
   enum condition:       {"新品、未使用": 0, "未使用に近い": 1, "目立った傷や汚れなし": 2, "やや傷や汚れあり": 3, "傷や汚れあり": 4, "全体的に状態が悪い": 5}
 
