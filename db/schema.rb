3d88f345bc547d27e1d8cb9200ff9c0d003f11cd
tunakan27 <takumi.naito27@gmail.com>
2019-04-17 11:45:20 +0900

    feat: active_strogeの導入

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,28 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_04_16_092843) do
+ActiveRecord::Schema.define(version: 2019_04_17_024433) do
+
+  create_table "active_storage_attachments", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+    t.string "name", null: false
+    t.string "record_type", null: false
+    t.bigint "record_id", null: false
+    t.bigint "blob_id", null: false
+    t.datetime "created_at", null: false
+    t.index ["blob_id"], name: "index_active_storage_attachments_on_blob_id"
+    t.index ["record_type", "record_id", "name", "blob_id"], name: "index_active_storage_attachments_uniqueness", unique: true
+  end
+
+  create_table "active_storage_blobs", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+    t.string "key", null: false
+    t.string "filename", null: false
+    t.string "content_type"
+    t.text "metadata"
+    t.bigint "byte_size", null: false
+    t.string "checksum", null: false
+    t.datetime "created_at", null: false
+    t.index ["key"], name: "index_active_storage_blobs_on_key", unique: true
+  end
 
   create_table "brand_categories", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.bigint "brand_id"
@@ -110,6 +131,7 @@ ActiveRecord::Schema.define(version: 2019_04_16_092843) do
     t.string "first_kana", null: false
     t.string "avatar"
     t.text "profile"
+    t.integer "phone_number"
     t.integer "birth_year"
     t.integer "birth_month"
     t.integer "birth_day"
@@ -122,8 +144,10 @@ ActiveRecord::Schema.define(version: 2019_04_16_092843) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "active_storage_attachments", "active_storage_blobs", column: "blob_id"
   add_foreign_key "brand_categories", "brands"
   add_foreign_key "brand_categories", "categories"
+  add_foreign_key "brands", "categories"
   add_foreign_key "carts", "items"
   add_foreign_key "carts", "users"
   add_foreign_key "comments", "items"
