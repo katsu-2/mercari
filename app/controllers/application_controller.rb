8fad4994488fb13995df326e9f8da844f6203a7e
大平勝也 <k8t2y9.cat@gmail.com>
2019-04-17 13:10:33 +0900

    fix: ログアウトパスを正常なものに変更

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,13 @@
 class ApplicationController < ActionController::Base
   before_action :basic_auth, if: :production?
+  before_action :configure_permitted_parameters, if: :devise_controller?
   protect_from_forgery with: :exception
+
+
+  protected
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:nickname, :family_name, :first_name, :family_kana, :first_kana, :bitrh_year, :bitrh_month, :bitrh_day])
+  end
 
   private
 
