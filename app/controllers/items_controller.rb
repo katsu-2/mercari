16eb6bbb63181ae04cd9387dcd109d21aaa6263f
tunakan27 <takumi.naito27@gmail.com>
2019-04-18 15:54:51 +0900

    feat: 商品出品を行える処理をitems_controllerに記述する

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -10,5 +10,23 @@ class ItemsController < ApplicationController
   end
 
   def new
+    @item = Item.new
+    @category = Category.where(parent_id: "0")
   end
+
+  def create
+    @item = Item.new(item_params)
+    if @item.save
+      redirect_to root_path
+    else
+      render :new
+    end
+  end
+
+  private
+    def item_params
+      params.require(:item).permit(:name, :description, :size, :condition, :delivery_fee, :delivery_date, :delivery_area, :delivery_way, :price, :status, images: []).merge(user_id: "1")
+    end
+
+
 end
