86619ee1d9c756f7a91c5e57bcb215b984a610ea
tunakan27 <takumi.naito27@gmail.com>
2019-04-18 15:53:21 +0900

    feat: カテゴリーの自己結合アソシエーションを組む

--- app/models/category.rb
+++ app/models/category.rb
@@ -2,4 +2,7 @@ class Category < ApplicationRecord
   has_many :items
   has_many :brand_categories
   has_many :brands, through: :brand_categories
+
+  belongs_to :parent, class_name: :Category
+  has_many :children, class_name: :Category, foreign_key: :parent_id
 end
