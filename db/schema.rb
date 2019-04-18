86699995bb6f3318f9a3a66ef69b87328a1e056f
tunakan27 <takumi.naito27@gmail.com>
2019-04-18 15:51:51 +0900

    feat: itemsテーブルの中に:statusを追加

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_04_17_024433) do
+ActiveRecord::Schema.define(version: 2019_04_18_061305) do
 
   create_table "active_storage_attachments", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "name", null: false
@@ -97,6 +97,7 @@ ActiveRecord::Schema.define(version: 2019_04_17_024433) do
     t.integer "delivery_date"
     t.integer "delivery_way"
     t.integer "delivery_area"
+    t.integer "status", null: false
     t.index ["user_id"], name: "index_items_on_user_id"
   end
 
