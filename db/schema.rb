8ef3fa3fe66fc3a9df9d99e7b9634d371d9426e7
tunakan27 <takumi.naito27@gmail.com>
2019-04-19 12:47:00 +0900

    feat: schemaの変更

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,6 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-
 ActiveRecord::Schema.define(version: 2019_04_18_061305) do
 
   create_table "active_storage_attachments", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
@@ -32,8 +31,7 @@ ActiveRecord::Schema.define(version: 2019_04_18_061305) do
     t.string "checksum", null: false
     t.datetime "created_at", null: false
     t.index ["key"], name: "index_active_storage_blobs_on_key", unique: true
-
-ActiveRecord::Schema.define(version: 2019_04_17_062631) do
+  end
 
   create_table "addresses", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "full_name", null: false
