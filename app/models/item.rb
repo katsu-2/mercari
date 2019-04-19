a4656c588716e071b5a8168b52cc8313a43fc416
tunakan27 <takumi.naito27@gmail.com>
2019-04-19 11:35:21 +0900

    feat: enumの文字列をシンボル型に変更

--- app/models/item.rb
+++ app/models/item.rb
@@ -9,28 +9,28 @@ class Item < ApplicationRecord
   # belongs_to :cart
   has_many_attached :images
 
-  enum condition:       {"新品、未使用": 0, "未使用に近い": 1, "目立った傷や汚れなし": 2, "やや傷や汚れあり": 3, "傷や汚れあり": 4, "全体的に状態が悪い": 5}
+  enum condition:       {brandNew: 0, nearUnused: 1, noDirt: 2, littleDirt: 3, dirt: 4, bad: 5}
 
-  enum delivery_way: {"未定": 0, "らくらくメルカリ便": 1, "ゆうメール": 2, "レターパック": 3, "普通郵便(定形、定形外)": 4, "クロネコヤマト": 5, "ゆうパック": 6, "クリックポスト": 7, "ゆうパケット": 8}
+  enum delivery_way: {undecided: 0, easyMercariPack: 1, postMail: 2, letterPack: 3, normalPost: 4, yamato: 5, postPack: 6, clickPost: 7, postPacket: 8}
 
-  enum delivery_fee: {"送料込み(出品者負担)": 0, "着払い(購入者負担)": 1}
+  enum delivery_fee: {postageIncluded: 0, cashOnDelivery: 1}
 
-  enum delivery_date: {"1~2日で発送": 0, "2~3日で発送": 1, "4~7日で発送": 2}
+  enum delivery_date: {fast: 0, normal: 1, slow: 2}
 
   enum delivery_area: {
-    "北海道": 1, "青森県": 2, "岩手県": 3, "宮城県": 4, "秋田県": 5, "山形県": 6, "福島県": 7,
-    "茨城県": 8, "栃木県": 9, "群馬県": 10, "埼玉県": 11, "千葉県": 12, "東京都": 13, "神奈川県": 14,
-    "新潟県": 15, "富山県": 16, "石川県": 17, "福井県": 18, "山梨県": 19, "長野県": 20,
-    "岐阜県": 21, "静岡県": 22, "愛知県": 23, "三重県": 24,
-    "滋賀県": 25, "京都府":26, "大阪府": 27, "兵庫県": 28, "奈良県": 29,"和歌山県": 30,
-    "鳥取県": 31, "島根県": 32, "岡山県": 33, "広島県": 34, "山口県": 35,
-    "徳島県": 36, "香川県": 37, "愛媛県": 38, "高知県": 39,
-    "福岡県": 40, "佐賀県": 41, "長崎県": 42, "熊本県": 43, "大分県": 44, "宮崎県": 45, "鹿児島県": 46, "沖縄県": 47
+    hokkaido: 1, aomori: 2, iwate: 3, miyagi: 4, akita: 5, yamagata: 6, fukushima: 7,
+    ibaraki: 8, tochigi: 9, gunma: 10, saitama: 11, chiba: 12, tokyo: 13, kanagawa: 14,
+    nigata: 15, toyama: 16, ishikawa: 17, fukui: 18, yamanashi: 19, nagano: 20,
+    gifu: 21, shizuoka: 22, aichi: 23, mie: 24,
+    shiga: 25, kyoto:26, osaka: 27, hyogo: 28, nara: 29,wakayama: 30,
+    tottori: 31, shimane: 32, okayama: 33, hiroshima: 34, yamaguchi: 35,
+    tokushima: 36, kagawa: 37, ehime: 38, kochi: 39,
+    fukuoka: 40, saga: 41, nagasaki: 42, kumamoto: 43, oita: 44, miyazaki: 45, kagoshima: 46, okinawa: 47
   }
 
-  enum size: {"XXS以下": 0, "XS(SS)": 1, "S": 2, "M": 3, "L": 4, "XL": 5, "2XL": 6, "3XL": 7, "4XL": 8, "5XL": 9, "FREESIZE": 10}
+  enum size: {xxs: 0, xs: 1, s: 2, m: 3, l: 4, xl: 5, xl2: 6, xl3: 7, xl4: 8, free: 9}
 
-  enum status: {"出品中": 0, "出品公開停止中": 1, "取引中": 2, "売り切れ": 3}
+  enum status: {exhibited: 0, stopped: 1, traiding: 2, sold: 3}
 
   validates :name, length: { maximum: 40 }, presence: true
   validates :description, length: { maximum: 1000 }, presence: true
