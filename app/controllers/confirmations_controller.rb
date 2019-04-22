55e4fce90bc3afc9d8abffeafb309d5db212790a
大平勝也 <k8t2y9.cat@gmail.com>
2019-04-22 15:39:10 +0900

    feat: 環境変数のセット

--- app/controllers/confirmations_controller.rb
+++ app/controllers/confirmations_controller.rb
@@ -1,4 +1,17 @@
 class ConfirmationsController < ApplicationController
+  require 'payjp'
+
   def show
+    @item = Item.find(params[:item_id])
+  end
+
+  def new
+    @item = Item.find(params[:item_id])
+  end
+
+  def create
+    @item = Item.find(params[:item_id])
+    Payjp.api_kei = 'PAYJP_PRIVATE_KEY'
+    charge = Payjp::Charge.create(amount: 3000, card: params['payjp-token'], currency: 'jpy')
   end
 end
