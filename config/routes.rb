b631e2e21c2ff014ac399538aaf37f76786af731
大平勝也 <k8t2y9.cat@gmail.com>
2019-04-25 18:11:25 +0900

    feat: ルーティングの追加

--- config/routes.rb
+++ config/routes.rb
@@ -17,6 +17,7 @@ Rails.application.routes.draw do
   end
 
 
+
   resources :users, only:[:show]
   resources :logouts, only: [:show]
 
@@ -25,7 +26,7 @@ Rails.application.routes.draw do
     resource :registrations, only: [:new, :create]
     resource :cellphones, only: [:new, :create]
     resource :addresses, only: [:new, :create]
-    resource :credits, only: [:index, :new, :create]
+    resources :credits, only: [:index, :new, :create]
     resource :identityinformations, only: [:show]
   end
 end
