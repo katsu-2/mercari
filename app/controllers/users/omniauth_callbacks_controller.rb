ccbf8510e76d4a8f2d1b2e59e1e3b6cb82800ebb
orix1944 <bf109.sai@gmail.com>
2019-04-26 13:01:09 +0900

    Removecommentout

--- app/controllers/users/omniauth_callbacks_controller.rb
+++ app/controllers/users/omniauth_callbacks_controller.rb
@@ -13,15 +13,15 @@ class Users::OmniauthCallbacksController < Devise::OmniauthCallbacksController
   private
 
   def callback_for(provider)
-      sns_info = SnsCredential.find_oauth(request.env['omniauth.auth'])
-      @user = sns_info[:user]
-      sns_id = sns_info[:sns_id]
-      if @user.persisted?
-        sign_in_and_redirect @user, event: :authentication #this will throw if @user is not activated
-      else
-        session['devise.sns_id'] = sns_id
-        render template: "devise/registrations/new"
-      end
+    sns_info = SnsCredential.find_oauth(request.env['omniauth.auth'])
+    @user = sns_info[:user]
+    sns_id = sns_info[:sns_id]
+    if @user.persisted?
+      sign_in_and_redirect @user, event: :authentication
+    else
+      session['devise.sns_id'] = sns_id
+      render template: "devise/registrations/new"
     end
+  end
 
 end
--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -1,36 +1,21 @@
 class Users::RegistrationsController < Devise::RegistrationsController
 
-
   def create
-      if params[:user][:email]
-
+    if params[:user][:email]
 
     password = Devise.friendly_token[0..8]
     params[:user][:password] = password
 
     end
 
-
     @sns_credential = SnsCredential.find(params[:id], session['devise.sns_id'])
-
 
     super
 
-     @sns_credential.user_id = @user.id
-     @sns_credential.save
-    # @sns_credential.save(user_id: @user.id)
-    # @sns_credential.user_id = User.
-    # @sns_credential.save
-
-
+    @sns_credential.user_id = @user.id
+    @sns_credential.save
 
   end
 
-
-  # private
-
-  # def user_params
-  # params.require(:user).permit(:family_name, :first_name, :family_kana, :first_kana).merge(email: @user.email, nickname: @user.nickname)
-  # end
 end
 
