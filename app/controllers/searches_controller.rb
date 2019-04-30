0c2e70ded421058c9a567f852875d323b86597ad
大平勝也 <k8t2y9.cat@gmail.com>
2019-04-30 13:10:25 +0900

    style: 検索一覧のスタイル調整

--- app/controllers/searches_controller.rb
+++ app/controllers/searches_controller.rb
@@ -2,8 +2,16 @@ class SearchesController < ApplicationController
   def index
     @search = Item.ransack(params[:q])
     @items = @search.result(distinct: true)
+    # binding.pry
   end
 
   def create
+    @search = Item.ransack(search_params)
+    @items = @search.result(distinct: true)
+  end
+
+  private
+  def search_params
+    params.require(:q).permit(:name_cont, :price ,:price_gteq, :price_lteq, condition_ids: [])
   end
 end
