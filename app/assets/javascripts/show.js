fd78a09e487c0f799321c59fb6985002074c2993
tunakan27 <takumi.naito27@gmail.com>
2019-05-02 12:32:32 +0900

    feat: item詳細ページの画像表示を変更する

--- app/assets/javascripts/show.js
+++ app/assets/javascripts/show.js
@@ -1,9 +1,13 @@
 $(function(){
   $("img.owl").hover(function(){
+    $(this).css("opacity", "1");
     var src = $(this).attr('src');
     $("img#show-main-image").attr({src});
     $("img#show-main-image").hide();
     $("img#show-main-image").fadeIn(50);
     return false;
+  },
+  function(){
+    $(this).css("opacity", "0.3");
   });
 });
